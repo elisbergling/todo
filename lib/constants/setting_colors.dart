a8d5e52b47c56a00d046db73ef0f01295a1f72a9
elis bergling <bergling.elis@gmail.com>
2020-11-08 11:45:50 +0100

    color update

--- lib/constants/setting_colors.dart
+++ lib/constants/setting_colors.dart
@@ -1,18 +1,21 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const SETTINGS_COLORS = [
+  Color(0xffff5252),
+  Color(0xff69f0ae),
+  Color(0xffffeb3b),
+  Color(0xffff4081),
   Color(0xffff6768),
   Color(0xff9ad3bc),
   Color(0xfff5b5fc),
   Color(0xff96f7d2),
-  Color(0xfff0f696),
   Color(0xfffcb1b1),
   Color(0xfff5b461),
   Color(0xff389393),
-  Color(0xfff3eac2),
-  Color(0xffec524b),
   Color(0xffadb36e),
-  Color(0xffebebeb),
+  Color(0xffec524b),
   Color(0xffac3f21),
   Color(0xff6e3b3b),
   Color(0xff364f6b),
@@ -20,11 +23,4 @@ const SETTINGS_COLORS = [
   Color(0xff16213e),
   Color(0xff1a1a2e),
   Color(0xff211717),
-  Color(0xff21e6c1),
-  Color(0xff43dde6),
-  Color(0xff9c1de7),
-  Color(0xff0000ff),
-  Color(0xffff0000),
-  Color(0xffffff00),
-  Color(0xff00ff00),
 ];
